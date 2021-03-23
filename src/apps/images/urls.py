486d4cf65b20e793cabbe87216ba5d6a438d88e6
Microsoft Learn Student <learn@contoso.com>
2021-03-22 20:01:02 -0500

    Paginacion de imagenes con AJAX

--- src/apps/images/urls.py
+++ src/apps/images/urls.py
@@ -3,14 +3,14 @@ from .views import (
     image_create, 
     image_detail,
     image_like,
-    ImageListView
+    image_list
 )
 
 
 app_name = 'images'
 
 urlpatterns = [
-    path('', ImageListView.as_view(), name='list'),
+    path('', image_list, name='list'),
     path('create/', image_create, name='create'),
     path('detail/<int:id>/<slug:slug>/', image_detail, name='detail'),
     path('like/', image_like, name='like'),
--- src/apps/images/views.py
+++ src/apps/images/views.py
@@ -1,19 +1,41 @@
-from django.http import JsonResponse
-from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
+from common.decorators.http import ajax_required
 from .models import Image
 from .forms import ImageCreateForm
-from django.conf import settings
 
 
 class ImageListView(ListView):
     model = Image
     paginate_by = 20
     template_name = 'images/image/list.html'
-    context_object_name = 'images'    
+    context_object_name = 'images'
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            # If the request is AJAX and the page is out of range
+            # return an empty page
+            return HttpResponse('', status=404)
+        # If page is out of range deliver last page of results
+        images = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request, 'images/image/list_ajax.html', { 'images': images })
+    return render(request,'images/image/list.html',{ 'images': images })
 
 
 @login_required
@@ -24,21 +46,20 @@ def image_create(request):
             new_item = form.save(commit=False)
             new_item.user = request.user
             new_item.save()
-            messages.success(request, 'Image added successfully')
             return redirect(new_item.get_absolute_url())
     else:
         # build form with data provided by the bookmarklet via GET
         form = ImageCreateForm(data=request.GET)
-
-    return render(request, 'images/image/create.html', { 'section': 'images', 'form': form })
+    return render(request, 'images/image/create.html', { 'form': form })
 
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    like_image_user = image.users_like.filter(username=request.user.username) .exists()
-    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image, 'like_image_user': like_image_user })
+    like_image_user = image.users_like.filter(username=request.user.username).exists()
+    return render(request, 'images/image/detail.html', { 'image': image, 'like_image_user': like_image_user })
 
 
+@ajax_required
 @login_required
 @require_POST
 def image_like(request):
